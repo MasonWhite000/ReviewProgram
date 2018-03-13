14098cd0143a1bcd6d6707db65fae9605001dfa2
MasonWhite000 <36765055+MasonWhite000@users.noreply.github.com>
2018-03-13 08:31:00 -0400

    Update review program.py

--- review program.py
+++ review program.py
@@ -33,22 +33,22 @@ valid = ["Movie", "Song", "Art", "Literature"]
 if media_type == ("Movie") or media_type == ("movie") or media_type == ("Film") or media_type == ("film"):
     valid.append(media_type)
     movies.append(review_material)
-    print (Movie[0:])
+    print (movies[0:])
     
 if media_type == ("Song") or media_type == ("song") or media_type == ("music") or media_type == ("Music"):
     valid.append(media_type)
     musics.append(review_material)
-    print (Music[0])
+    print (musics[0])
     
 if media_type == ("Art") or media_type == ("art") or media_type == ("Painting") or media_type == ("painting") or media_type == ("Picture") or media_type == ("picture") or media_type == ("Drawling") or media_type == ("drawling"):
     valid.append(media_type)
     arts.append(review_material)
-    print (Art[0:])
+    print (arts[0:])
     
 if media_type == ("Literature") or media_type == ("literature") or media_type == ("Book") or media_type == ("book"):
     valid.append(media_type)
     literature.append(review_material)
-    print (Literature[0:])
+    print (literature[0:])
 
 if media_type not in valid:
     print("That type of media isn't on our records!.")
